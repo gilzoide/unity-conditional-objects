c1e34c186c4c72a8c614024804674cfb9fcd32b1
gilzoide <gilzoide@gmail.com>
2023-09-09 18:34:43 -0300

    Add GameObjectOrComponentAttribute

---
+++ Runtime/Internal/GameObjectOrComponentAttribute.cs
@@ -0,0 +1,10 @@
+using System;
+using UnityEngine;
+
+namespace Gilzoide.ConditionalObjects
+{
+    [AttributeUsage(AttributeTargets.Field)]
+    public class GameObjectOrComponentAttribute : PropertyAttribute
+    {
+    }
+}
